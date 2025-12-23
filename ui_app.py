08664c3ee4fa100e6ab6cd7bbc17d20b0092879a
HimanshuVaidya25 <165823711+HimanshuVaidya25@users.noreply.github.com>
2025-12-23 15:03:20 +0530

    ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 #from summarizer import summarize_text
-from sentiment_filter import is_positive
+#from sentiment_filter import is_positive
 from news_fetcher import fetch_news
 from translator import translate_text
 from tts_converter import text_to_speech
@@ -202,5 +202,6 @@ elif section == "Login":
     password = st.text_input("Password", type="password")
     if st.button("Login"):
         st.success(f"✅ Logged in as {email}")
+
 
 
